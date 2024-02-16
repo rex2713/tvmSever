c33b3005b305fe7492a8871bc6f52d5efb8d9c09
rex2713 <rex2713@gmail.com>
2024-02-16 13:05:28 +0800

    opt

--- config/multer-court.js
+++ config/multer-court.js
@@ -7,7 +7,7 @@ const Default_Upload = process.env.Default_Upload;
 const storage = multer.diskStorage({
   //檔案儲存位置
   destination: (req, file, cb) => {
-    cb(null, Default_Upload); //存public
+    cb(null, Default_Upload);
   },
   //檔案儲存名稱
   filename: (req, file, cb) => {
