559d7ccd8cdcd8e8d13a039d2af960953da686e8
rex2713 <rex2713@gmail.com>
2024-02-16 15:42:53 +0800

    copy

--- routes/court-route.js
+++ routes/court-route.js
@@ -3,14 +3,7 @@ const router = express.Router();
 const Court = require("../models").court;
 const courtValidation = require("../validation").courtValidation;
 const fs = require("fs");
-
-fs.readdir("/var/data", (err, files) => {
-  if (err) {
-    console.error("Error reading render disk:", err);
-    return;
-  }
-  console.log(files);
-});
+const path = require("path");
 
 router.use((req, res, next) => {
   console.log("正在接受一個court-route的相關請求");
@@ -20,6 +13,28 @@ router.use((req, res, next) => {
 //獲得資料庫中所有球場資料
 router.get("/", async (req, res) => {
   console.log("正在獲取球場資料");
+
+  //測試複製圖檔
+  const renderDisk = "/var/data";
+  const publicFolder = "./images";
+  fs.readdir("/var/data", (err, files) => {
+    if (err) {
+      console.error("Error reading render disk:", err);
+      return;
+    }
+    console.log(files);
+    files.forEach((file) => {
+      const copyPath = path.join(renderDisk, file);
+      const pastPath = path.join(publicFolder, file);
+      fs.copyFile(copyPath, pastPath, (err) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log("success copy");
+        }
+      });
+    });
+  });
 
   try {
     let courtFound = await Court.find({}).exec();
