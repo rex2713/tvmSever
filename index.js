aef316e5f1d61f621b88e344f5a6f33acb5f9ea4
rex2713 <rex2713@gmail.com>
2024-02-17 11:39:05 +0800

    預檢請求

--- index.js
+++ index.js
@@ -26,12 +26,30 @@ mongoose
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public")); //公開public資料夾（提供前端訪問權限）
+
+//跨域鬆綁
 app.use(
   cors({
     origin: ["http://localhost:5173", "https://tvm-0wj4.onrender.com"],
   })
-); //跨域鬆綁
-app.use(express.static("public")); //公開public資料夾（提供前端訪問權限）
+);
+//處理預檢請求
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://tvm-0wj4.onrender.com");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+  next();
+});
 
 //路由
 app.use("/tvm/user", authRoute);
